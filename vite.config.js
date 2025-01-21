66b1344c19e9181f3afca561ab12ae2b385ac00b
Vansh Rajput <135845531+vanshrajput099@users.noreply.github.com>
2025-01-21 18:43:40 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:5173
+  base: "/visual-product-manager-frontend/",
+  server: {
+    port: 5173
   }
 })
